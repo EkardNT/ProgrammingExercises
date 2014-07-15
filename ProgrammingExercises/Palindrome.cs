3ec92aa34f4fc7a5e0b3ba998975206ab62dad4b
Drake Tetreault <ekardnt@ekardnt.com>
2014-07-15 10:42:03 -0700

    Initial commit with command dispatcher and palindrome checker.

---
+++ ProgrammingExercises/Palindrome.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ProgrammingExercises
+{
+	public static class Palindrome
+	{
+		public static void Execute(string word, bool fast = false)
+		{
+			bool isPalindrome = fast ? TestFast(word) : TestSlow(word);
+			Console.WriteLine("\"{0}\" {1} a palindrome.", word, isPalindrome ? "is" : "is not");
+		}
+
+		private static bool TestFast(string word)
+		{
+			for(int i = 0; i < word.Length / 2; i++)
+			{
+				if (word[i] != word[word.Length - 1 - i])
+					return false;
+			}
+			return true;
+		}
+
+		private static bool TestSlow(string word)
+		{
+			return word == word.Reverse();
+		}
+
+		private static string Reverse(this string @this)
+		{
+			var chars = new char[@this.Length];
+			for (int i = 0; i < @this.Length; i++)
+				chars[i] = @this[@this.Length - 1 - i];
+			return new string(chars);
+		}
+	}
+}
---
+++ ProgrammingExercises/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingExercises
+{
+	static class Program
+	{
+		private static readonly Dictionary<Type, Func<string, object>> parsers = new Dictionary<Type, Func<string, object>>
+		{
+			{typeof(string), str => str},
+			{typeof(byte), str => byte.Parse(str)},
+			{typeof(sbyte), str => sbyte.Parse(str)},
+			{typeof(short), str => short.Parse(str)},
+			{typeof(ushort), str => ushort.Parse(str)},
+			{typeof(int), str => int.Parse(str)},
+			{typeof(uint), str => uint.Parse(str)},
+			{typeof(long), str => long.Parse(str)},
+			{typeof(ulong), str => ulong.Parse(str)},
+			{typeof(bool), str => bool.Parse(str)}
+		};
+
+		public static void Main(string[] args)
+		{
+			string input;
+			while (!string.Equals(input = Console.ReadLine(), "quit"))
+			{
+				var parts = input.Split();
+				var commandName = parts[0];
+				var commandArgs = parts.Skip(1).Take(parts.Length - 1).ToArray();
+				try
+				{
+					Dispatch(commandName, commandArgs);
+				}
+				catch (Exception e)
+				{
+#if DEBUG
+					Console.WriteLine(e);
+#else
+					Console.WriteLine("Unhandled error occurred: {0}.", e.Message);
+#endif
+				}
+			}
+		}
+
+		private static void Dispatch(string commandName, string[] commandArgs)
+		{
+			var executeMethod = GetExecuteMethod(commandName);
+			if(executeMethod == null)
+				throw new ArgumentException(string.Format("No executable method found for command \"{0}\".", commandName));
+			var parameters = ParseArguments(executeMethod.GetParameters(), commandArgs);
+			executeMethod.Invoke(null, parameters);
+		}
+
+		private static MethodInfo GetExecuteMethod(string commandName)
+		{
+			return typeof(Program).Assembly.DefinedTypes
+				.Where(type => type.IsClass)
+				.Where(type => type.IsPublic)
+				.Where(type => type.Name.Equals(commandName, StringComparison.InvariantCultureIgnoreCase))
+				.Select(type => type.DeclaredMethods
+					.Where(method => method.IsStatic)
+					.Where(method => method.Name.Equals("Execute"))
+					.First())
+				.FirstOrDefault();
+		}
+
+		private static object[] ParseArguments(ParameterInfo[] parameters, string[] arguments)
+		{
+			return parameters.Select((param, index) =>
+			{
+				if (index >= arguments.Length)
+				{
+					if (param.IsOptional)
+						return Type.Missing;
+					else
+						throw new ArgumentException(string.Format("Required parameter \"{0}\" is missing.", param.Name));
+				}
+				return parsers[param.ParameterType](arguments[index]);
+			}).ToArray();
+		}
+	}
+}
