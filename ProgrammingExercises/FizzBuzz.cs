7d20190920978f698a8be70d810d135126957b76
Drake Tetreault <ekardnt@ekardnt.com>
2014-07-15 15:25:16 -0700

    FizzBuzz.

---
+++ ProgrammingExercises/FizzBuzz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ProgrammingExercises
+{
+	public static class FizzBuzz
+	{
+		public static void Execute(int min = 1, int max = 100)
+		{
+			for (int i = min; i <= max; i++)
+			{
+				if (i % 5 == 0 && i % 3 == 0)
+					Console.WriteLine("FizzBuzz");
+				else if (i % 5 == 0)
+					Console.WriteLine("Buzz");
+				else if (i % 3 == 0)
+					Console.WriteLine("Fizz");
+				else
+					Console.WriteLine(i);
+			}
+		}
+	}
+}
